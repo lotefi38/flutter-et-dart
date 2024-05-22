844208e280be2471ba1c75b05681b4cdb1b3ddea
lotefi38 <lotefimohdeb@outlook.fr>
2024-05-22 15:26:29 +0200

    commit fifth

--- lib/widgets/home/ads_widget.dart
+++ lib/widgets/home/ads_widget.dart
@@ -1,46 +1,80 @@
 import 'dart:async';
-
+ 
 import 'package:flutter/material.dart';
-
+ 
 // class du widget
 class AdsWidget extends StatefulWidget {
   AdsWidget({super.key});
-
+ 
   /*
     dart:
-      List : équivalent à un array indicé 
+      List : équivalent à un array indicé
       Map : équivalent à un objet  JS   (clé - valeure)
-
+ 
     visibilité des membres :
-      pas de visibilité protected 
+      pas de visibilité protected
       quand il a une propriété préfixé de _ ceci veut dire qu'elle est protected (ex _maPropriéte)
   */
-
+ 
   // Liste des Map
   final List<Map> _items = [
     {'img': 'Ad1.jpg', 'text': 'Text1'},
     {'img': 'Ad2.jpg', 'text': 'Text2'},
     {'img': 'Ad3.jpg', 'text': 'Text3'},
   ];
-
+ 
   // idnice
-  final int _index = 0;
-
+  int _index = 0;
+ 
   // minuteur
   // ? : valeur nulle
   Timer? _timer;
-
+ 
   @override
   State<AdsWidget> createState() => _AdsWidgetState();
 }
-
+ 
 // class de l'état du widget qui hérite de la class du Widget    , methode build responsable pour l'affichage
 class _AdsWidgetState extends State<AdsWidget> {
+  /*
+    cycle de vie d'un widget
+      initState : affichage du widget
+      dispose : désaffichage du widget
+  */
+  @override
+  void initState() {
+    super.initState();
+    //declencher le timer
+    widget._timer = Timer.periodic(const Duration(seconds: 2), _setTimer);
+  }
+ 
+  void _setTimer(Timer timer) {
+    // setState : permet de modifier uné tat et de décencher la mise à jour visuelle du widget
+    setState(() {
+      if (widget._index < widget._items.length - 1) {
+        widget._index++;
+      } else {
+        widget._index = 0;
+      }
+    });
+  }
+ 
+  @override
+  void dispose() {
+    super.dispose();
+    //stopper le minuteur
+    // null coaslesing : utilisation ?
+    // ! : permet de définir que la valeur n'est pas nulle
+    widget._timer!.cancel();
+  }
+ 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Image.asset('assets/img/ad1.jpeg'),
+        // le mot_clé widget permet d'accéder à la classe du wiget à partir de la classe de l'état du widget
+        Image.asset('assets/img/${widget._items[widget._index]['img']}'),
+        Text(widget._items[widget._index]['text']),
       ],
     );
   }
